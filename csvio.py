d3680a4bb2eff4b2c1253c61526f0bc3969573f5
mritter-tgm <mritter@student.tgm.ac.at>
2016-05-05 18:38:50 +0000

    fixed some shit 2

--- csvio.py
+++ csvio.py
@@ -5,7 +5,6 @@ class CSV:
 
     @staticmethod
     def read(filename):
-
         with open(filename, "r") as file:
             reader = csv.reader(file, delimiter=";")
             lines = [line for line in reader]
@@ -13,7 +12,6 @@ class CSV:
 
     @staticmethod
     def write(filename, header, lines):
-
         with open(filename, "w") as file:
             writer = csv.writer(file)
             writer.writerow(header)
